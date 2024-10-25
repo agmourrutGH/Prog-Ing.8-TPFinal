5effd0e72572801f27d1146260266a3ada8065e5
latorresGH <laureano26112001@hotmail.com>
2024-10-25 01:03:32 -0300

    finalizamos index

--- LestCook/platforms/android/app/src/main/assets/www/index.js
+++ LestCook/platforms/android/app/src/main/assets/www/index.js
@@ -1,11 +1,3 @@
 function cambiarDePagina(){
-<<<<<<< HEAD
-<<<<<<< HEAD
-    window.location.href='mvc/vista/header.html';
-=======
-    window.location.href='mvc/vista/login.html';
->>>>>>> 2c56ba1f331da900ef509bb1af2a364006afdf62
-=======
     window.location.href='mvc/vista/index.html';
->>>>>>> index-laure
  }
